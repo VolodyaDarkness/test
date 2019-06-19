a93d97cfe897f49ca8c875ae6c26ed0436471217
Sergey <1705home.com>
2019-06-19 18:25:12 +0300

    first comment

---
+++ scripts/main.js
@@ -0,0 +1,34 @@
+'use strict';
+
+let $searchForm = $('#search-form');
+let $bookList = $('#book-list');
+let arrBooks = [];
+
+$searchForm.on("submit", function(event){
+	event.preventDefault();
+	let query = $(this).find("[name = 'srch-term']").val().replace(/\s/g, "+");
+
+	getBooks(query);
+});
+
+function getBooks(query){
+	var server = 'https://www.googleapis.com/books/v1/volumes';
+
+	$.ajax({
+		url: server,
+		method: "GET",
+		data: `q=${query}`
+	}).done(function (response){
+		arrBooks = response.items;
+		$bookList.empty();
+		arrBooks.forEach(function (book){
+			$('<a href="">').addClass('list-group-item')
+			.text(book.volumeInfo.title)
+			.attr('data-id', book.id)
+			.appendTo($bookList);
+		})
+	}).fail(function (error) {
+		console.log(error);
+	})
+}
+
